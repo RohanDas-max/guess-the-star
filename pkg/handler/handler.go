d3fbeee74fccd1295fd1d7b92b022467c36a1c5c
rohandas-max <rohandasd0@gmail.com>
2022-02-21 21:44:56 +0530

    removed context

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"context"
 	"encoding/json"
 
 	"github.com/rohandas-max/GuessTheStar/pkg/utils"
@@ -14,11 +13,11 @@ type TrendingRepo struct {
 	Language   string `json:"language"`
 }
 
-func GetTrendingRepos(ctx context.Context, flags map[string]string) ([]TrendingRepo, error) {
+func GetTrendingRepos(flags map[string]string) ([]TrendingRepo, error) {
 	lang := flags["lang"]
 	since := flags["since"]
 	var url string = "https://gh-trending-api.herokuapp.com/repositories/" + lang + "?since=" + since
-	bytes, err := utils.Get(ctx, url)
+	bytes, err := utils.Get(url)
 	if err != nil {
 		return []TrendingRepo{}, err
 	}
--- pkg/handler/handler_test.go
+++ pkg/handler/handler_test.go
@@ -1,15 +1,11 @@
 package handler
 
 import (
-	"context"
 	"testing"
 )
 
 func TestGetTrendingRepos(t *testing.T) {
-	ctx, cncl := context.WithCancel(context.Background())
-	defer cncl()
 	type args struct {
-		ctx   context.Context
 		flags map[string]string
 	}
 	tests := []struct {
@@ -20,23 +16,14 @@ func TestGetTrendingRepos(t *testing.T) {
 		{
 			name: "with data",
 			args: args{
-				ctx:   ctx,
 				flags: map[string]string{"lang": "go", "since": "weekly"},
-			},
-			wantErr: false,
-		},
-		{
-			name: "pass2",
-			args: args{
-				ctx:   ctx,
-				flags: map[string]string{"lang": "rust", "since": "weekly"},
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if _, got1 := GetTrendingRepos(tt.args.ctx, tt.args.flags); (got1 != nil) != tt.wantErr {
+			if _, got1 := GetTrendingRepos(tt.args.flags); (got1 != nil) != tt.wantErr {
 				t.Errorf("GetTrendingRepos() = %v, want %v", got1, tt.wantErr)
 			}
 		})
