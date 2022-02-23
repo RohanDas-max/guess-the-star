6db6001fb6898b611e753f297655fe2790d17b82
rohandas-max <rohandasd0@gmail.com>
2022-02-23 22:47:17 +0530

    modified

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -2,11 +2,8 @@ package controller
 
 import (
 	"context"
-	"errors"
-	"fmt"
 
 	"github.com/rohandas-max/GuessTheStar/pkg/handler"
-	"github.com/rohandas-max/GuessTheStar/pkg/utils"
 )
 
 func Controller(ctx context.Context, flags map[string]string) error {
@@ -14,44 +11,9 @@ func Controller(ctx context.Context, flags map[string]string) error {
 	case <-ctx.Done():
 		return ctx.Err()
 	default:
-		repos, err := handler.GetTrendingRepos(flags)
-		if err != nil {
+		if err := handler.StartTheGame(flags); err != nil {
 			return err
-		}
-		if len(repos) > 4 {
-			count := GettingTotalScore(repos)
-			if count > 3 {
-				fmt.Println("LETS GO!!! you won the game!")
-			} else {
-				fmt.Println("Opps!! Better Luck next Time")
-			}
-		} else {
-			return errors.New("not enough trending repos to play the game")
 		}
 	}
 	return nil
-}
-
-func GettingTotalScore(repos []handler.TrendingRepo) int {
-	var count int
-	for i := 0; i < 5; i++ {
-		repo := repos[i]
-		fmt.Printf("Round:%d\tRepo:%s/%s\nlanguage:%s\n", i+1, repo.Username, repo.Name, repo.Language)
-		var input int
-		fmt.Println("GUESS THE STAR")
-		fmt.Scanf("%d", &input)
-		if checkScoreFromUserInput(repo.TotalStars, input) > 0 {
-			count++
-		}
-	}
-	return count
-}
-
-func checkScoreFromUserInput(TotalStars, input int) int {
-	if TotalStars > 0 {
-		if utils.IsCorrectGuess(TotalStars, input) {
-			return 1
-		}
-	}
-	return 0
 }
--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -4,145 +4,42 @@ import (
 	"context"
 	"testing"
 	"time"
-
-	"github.com/rohandas-max/GuessTheStar/pkg/handler"
 )
 
 func TestController(t *testing.T) {
-	ctxWithTimeout, cancle := context.WithTimeout(context.Background(), 1*time.Microsecond)
-	defer cancle()
-	ctxWithCancel, cancle := context.WithCancel(context.Background())
-	defer cancle()
-
-	type args struct {
-		ctx   context.Context
-		flags map[string]string
-	}
+	ctxWithdeadline, cancel := context.WithTimeout(context.Background(), 1*time.Microsecond)
+	defer cancel()
+	ctxWIthcancel, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	tests := []struct {
 		name    string
-		args    args
+		ctx     context.Context
+		flags   map[string]string
 		wantErr bool
 	}{
 		{
-			name: "fail with context deadline",
-			args: args{
-				ctx:   ctxWithTimeout,
-				flags: map[string]string{"lang": "go", "since": "weekly"},
-			},
+			name:    "return error ctx deadline",
+			ctx:     ctxWithdeadline,
+			flags:   map[string]string{"lang": "rust", "since": "weekly"},
 			wantErr: true,
 		},
 		{
-			name: "passed with nil error",
-			args: args{
-				ctx:   ctxWithCancel,
-				flags: map[string]string{"lang": "go", "since": "weekly"},
-			},
+			name:    "return nil error ctx cancel",
+			ctx:     ctxWIthcancel,
+			flags:   map[string]string{"lang": "rust", "since": "weekly"},
 			wantErr: false,
 		},
+		{
+			name:    "return error because of wrong flag",
+			ctx:     ctxWIthcancel,
+			flags:   map[string]string{"": "", "since": ""},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := Controller(tt.args.ctx, tt.args.flags); (err != nil) != tt.wantErr {
+			if err := Controller(tt.ctx, tt.flags); (err != nil) != tt.wantErr {
 				t.Errorf("Controller() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func Test_GettingTotalScore(t *testing.T) {
-	type args struct {
-		repos []handler.TrendingRepo
-		want  int
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "passed with empty values",
-			args: args{
-				repos: []handler.TrendingRepo{{
-					Username:   "",
-					Name:       "",
-					TotalStars: 0,
-					Language:   "",
-				}, {}, {}, {}, {}},
-				want: 0,
-			},
-		},
-		{
-			name: "passed with values",
-			args: args{
-				repos: []handler.TrendingRepo{{
-					Username:   "go",
-					Name:       "go",
-					TotalStars: 3000,
-					Language:   "go",
-				}, {
-					Username:   "rust",
-					Name:       "rust",
-					TotalStars: 1000,
-					Language:   "rust",
-				}, {
-					Username:   "js",
-					Name:       "js",
-					TotalStars: 10000,
-					Language:   "js",
-				}, {
-					Username:   "python",
-					Name:       "python",
-					TotalStars: 9000,
-					Language:   "python",
-				}, {
-					Username:   "scala",
-					Name:       "scala",
-					TotalStars: 2000,
-					Language:   "scala",
-				}},
-				want: 0,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := GettingTotalScore(tt.args.repos)
-			if got != tt.args.want {
-				t.Error("got != want", got, tt.args.want)
-			}
-		})
-	}
-}
-
-func Test_checkUserInputAndScore(t *testing.T) {
-	tests := []struct {
-		name       string
-		totalStars int
-		input      int
-		want       int
-	}{
-		{
-			name:       "should return 0 because of wrong input",
-			totalStars: 123,
-			input:      1,
-			want:       0,
-		},
-		{
-			name:       "should return 1 because of right input",
-			totalStars: 123,
-			input:      123,
-			want:       1,
-		},
-		{
-			name:       "should return 1 because of empty value",
-			totalStars: 0,
-			input:      0,
-			want:       0,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := checkScoreFromUserInput(tt.totalStars, tt.input); got != tt.want {
-				t.Errorf("checkUserInputAndScore() = %v, want %v", got, tt.want)
 			}
 		})
 	}
