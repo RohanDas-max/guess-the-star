ddd9c9a1c075c3dd6f996336736e3d7103a6f94d
rohandas-max <rohandasd0@gmail.com>
2022-02-24 20:57:49 +0530

    made a variable for tolerance value

--- pkg/utils/isCorrectGuess.go
+++ pkg/utils/isCorrectGuess.go
@@ -1,7 +1,8 @@
 package utils
 
 func IsCorrectGuess(Totalstar, input int) bool {
-	t := ((Totalstar * 10) / 100)
+	tolerance := 10
+	t := ((Totalstar * tolerance) / 100)
 	if input <= Totalstar+t && input >= Totalstar-t {
 		return true
 	}
